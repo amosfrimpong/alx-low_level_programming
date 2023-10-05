32ffaa6bc21b438f2c0864aeabc318b21b8c617a
amosfrimpong <amosfrimpong745@gmail.com>
2023-10-05 06:52:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print a text
+ *
+ * Return : 0 on success
+ *
+ */
+
+int main (void)
+{
+	puts(" \"Programming is like building a multilingual puzzle,\n");
+	return (0);
+}
