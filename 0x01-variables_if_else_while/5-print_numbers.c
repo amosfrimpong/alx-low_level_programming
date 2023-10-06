9e17bdef695864d1356b7f09350e80b70520989b
amosfrimpong <amosfrimpong745@gmail.com>
2023-10-06 11:56:18 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- The entry point to the program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar (i);
+	}
+
+	putchar ('\n');
+	return (0);
+}
