c37198402b634d749b06f6dc5f6091bdd795e1de
amosfrimpong <amosfrimpong745@gmail.com>
2023-10-11 09:06:32 -0700

    Quick correction and Adding a new file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,23 +1,10 @@
-#include <stdio.h>
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
 
-int main(void)
-{
-        int a, b, c;
-        int largest;
+int largest_number(int a, int b, int c);
 
-        a = 972;
-        b = -98;
-        c = 0;
 
-        largest = largest_number(a, b, c);
 
-        printf("%d is the largest number\n", largest);
 
-        return (0);
-}
+#endif
