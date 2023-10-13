d70c9b815255c5aa3bab31ac8fd1d85ea032feaa
amosfrimpong <amosfrimpong745@gmail.com>
2023-10-12 17:31:49 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_numbers: priunt Numbers
+ *
+ *Return: Nothing
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
