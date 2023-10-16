1291a8c5e5b1f7529a3f927aa79d81a956b2426c
amosfrimpong <amosfrimpong745@gmail.com>
2023-10-16 13:05:26 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts- Print a string that is pass to the function
+ *
+ * @str: a pointer to the string
+ */
+
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
